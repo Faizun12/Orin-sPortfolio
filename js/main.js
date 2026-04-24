125339727a9c9190c5c28618f4a44c231d1d8b5a
Faizun Nesa Orin <22201139@uap-bd.edu>
2026-04-25 00:09:42 +0600

    fixed ui glitch

--- js/main.js
+++ js/main.js
@@ -100,15 +100,20 @@
 
 
     // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+    var portfolioIsotope;
+    $(window).on('load', function() {
+        portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
     });
     $('#portfolio-flters li').on('click', function () {
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        if (portfolioIsotope) {
+            portfolioIsotope.isotope({filter: $(this).data('filter')});
+        }
     });
 
 
